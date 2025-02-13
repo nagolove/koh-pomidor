7c1ccf753f43495820dac69d0b24b07fbb09ec99
nagolove <dekar.own@gmail.com>
2025-02-13 21:47:23 +0300

    Минимальный код. Работает.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 // vim: set colorcolumn=85
 // vim: fdm=marker
 
-#include "koh_script.h"
 #include "koh_stages.h"
 #if defined(PLATFORM_WEB)
 #include <emscripten/emscripten.h>
@@ -25,118 +24,118 @@
 #include <memory.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "koh_timerman.h"
 
 // }}}
 
 Sound snd_beep;
 Color color_background_clear = GRAY;
-static double last_time = 0.;
 static Camera2D cam = {
     .zoom = 1.,
 };
+TimerMan *tm = NULL;
+const int scr_w = 300, scr_h = 60;
+float times[] = {
+    25. * 60., 
+    5 * 60.
+};
+Color colors[] = {
+    RED,
+    GREEN,
+};
+int i = 0;
+bool stop = false;
 
+static bool tmr_on_update(struct Timer *tmr);
+static void tmr_on_stop(struct Timer *tmr);
+
+static void init() {
+    stop = false;
+    timerman_add(tm, (TimerDef) {
+        .duration = times[i],
+        .on_update = tmr_on_update,
+        .on_stop = tmr_on_stop,
+    });
+}
+
+static void tmr_on_stop(struct Timer *tmr) {
+    i = (i + 1) % 2;
+    init();
+    /*trace("tmr_on_stop: i %d\n", i);*/
+}
+
+static bool tmr_on_update(struct Timer *tmr) {
+    /*trace("tmr_on_update: amount %f, i %d\n", tmr->amount, i);*/
+    DrawRectangle(0, 0, scr_w * (1. - tmr->amount), scr_h, colors[i]);
+    const float thick = 4;
+    DrawRectangleLinesEx((Rectangle) { 0., 0, scr_w, scr_h, }, thick, BLACK);
+    return stop;
+}
 static void update(void) {
     if (IsKeyPressed(KEY_SPACE)) {
+        /*trace("update: SPACE pressed\n");*/
         PlaySound(snd_beep);
+        stop = true;
     }
 
     BeginDrawing();
     BeginMode2D(cam);
     ClearBackground(color_background_clear);
+    timerman_update(tm);
     EndMode2D();
     EndDrawing();
 }
 
-#if !defined(PLATFORM_WEB)
-
 void sig_handler(int sig) {
     printf("sig_handler: %d signal catched\n", sig);
-    /*
-    // XXX:
-    if (__STDC_VERSION__ >=201710L) {
-    } else
-        printf("sig_handler: %s signal catched\n", strsignal(sig));
-    */
     koh_backtrace_print();
     KOH_EXIT(EXIT_FAILURE);
 }
-#endif
 
 int main(int argc, char **argv) {
-#if !defined(PLATFORM_WEB)
     signal(SIGSEGV, sig_handler);
-#endif
 
     SetTraceLogCallback(koh_log_custom);
 
     koh_hashers_init();
     logger_init();
 
-    const char *wnd_name = "lapsha";
-
     SetTraceLogLevel(LOG_WARNING);
 
-#ifdef PLATFORM_WEB
-    SetConfigFlags(FLAG_MSAA_4X_HINT);  // Set MSAA 4X hint before windows creation
-    InitWindow(screenWidth_web, screenHeight_web, wnd_name);
-    SetTraceLogLevel(LOG_ALL);
-#else
-    //SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_FULLSCREEN_MODE);  // Set MSAA 4X hint before windows creation
     SetConfigFlags(
         FLAG_WINDOW_UNDECORATED |
         FLAG_WINDOW_TOPMOST |
         FLAG_WINDOW_ALWAYS_RUN
     );  
        
-    SetWindowPosition(40, 40);
-    InitWindow(300, 60, wnd_name);
-    SetWindowPosition(40, 40);
+    InitWindow(scr_w, scr_h, "pomidor");
 
-    SetTraceLogLevel(LOG_ALL);
-    // FIXME: Работает только на моей конфигурации, сделать опцией
-    // К примеру отрабатывать только на флаг -DDEV
-#ifndef LAPTOP
-    SetWindowPosition(GetMonitorPosition(1).x, 0);
-#endif
-    //dotool_setup_display(testing_ctx);
-#endif
+    /*SetWindowPosition(40, 40);*/
+    /*SetWindowPosition(GetMonitorPosition(1).x, 0);*/
 
     SetExitKey(KEY_NULL);
 
-    logger_register_functions();
-
     koh_common_init();
 
-    // XXX: Требуется включение и выключение
     InitAudioDevice();
 
     snd_beep = LoadSound("assets/beepgen.wav");
 
     koh_render_init();
 
-    // stage_init(ss);
+    tm = timerman_new(40, "timers");
+    init();
 
-    last_time = GetTime();
-
-#if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(update, 60, 1);
-#else
-    //dotool_send_signal(testing_ctx);
-
-    SetTargetFPS(120 * 3);
+    SetTargetFPS(120);
     while (!WindowShouldClose() && !koh_cmn()->quit) {
         update();
     }
 
-#endif
-
+    timerman_free(tm);
     koh_music_shutdown();       // добавить в систему инициализации
     koh_render_shutdown();// добавить в систему инициализации
     koh_common_shutdown();// добавить в систему инициализации
     CloseWindow();// добавить в систему инициализации
-
-    //dotool_free(testing_ctx);
     logger_shutdown();
 
     return EXIT_SUCCESS;
